b1b545245079952cfd1aa238055590a0b2292c71
SHANJAIKUMAAR <shanjaikumaarsellamuthu@gmail.com>
2024-05-22 00:05:04 +0530

    updated cors packages

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const productsRoutes = require('./routes/products')
 const userRoutes = require('./routes/user')
 
 //express app
 const app = express()
-
+app.use(cors());
 // middleware
 app.use(express.json())
 
