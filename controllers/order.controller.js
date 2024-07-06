6e0df09fd7a031ed564cb6cf24a6ff8afb4ea5c1
sanjithrk06 <sanjithkarthikeyanr@gmail.com>
2024-07-06 18:55:14 +0530

    Order Controller completed

---
+++ controllers/order.controller.js
@@ -0,0 +1,43 @@
+const orderService = require("../services/order.service");
+
+const createOrder = async(req, res) => {
+    const user = req.user;
+
+    try {
+        let createdOrder = await orderService.createOrder(user, req.body);
+
+        return res.status(201).send(createdOrder);
+    } catch (error) {
+        return res.status(500).send({error: error.message});
+    }
+}
+
+const findOrderById = async(req, res) => {
+    const user = req.user;
+
+    try {
+        let order = await orderService.findOrderById(req.params.id);
+
+        return res.status(201).send(order);
+    } catch (error) {
+        return res.status(500).send({error: error.message});
+    }
+}
+
+const orderHistory = async(req, res) => {
+    const user = req.user;
+
+    try {
+        let order = await orderService.usersOrderHistory(user._id);
+
+        return res.status(201).send(order);
+    } catch (error) {
+        return res.status(500).send({error: error.message});
+    }
+}
+
+module.exports = {
+    createOrder,
+    findOrderById,
+    orderHistory
+}
